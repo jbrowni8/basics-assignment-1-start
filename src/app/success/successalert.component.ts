b48eb8039fca0e86b2fdd8dd8581916742b7145d
James Browning <jbrowni8@jamess-mbp.speedport.ip>
2018-10-24 07:59:59 +0200

    ship

--- src/app/success/successalert.component.ts
+++ src/app/success/successalert.component.ts
@@ -3,7 +3,15 @@ import { Component } from "@angular/core";
 @Component ({
     selector : 'app-successalert', 
     templateUrl : './successalert.component.html', 
-    styleUrls: ['./successalert.component.css']
+    styles: [
+        `
+        p {
+            padding: 20px: 
+            background-color: lightgreen:
+            border: 1px solid green;
+        }
+        `
+    ]
 
 })
 export class SuccessAlert {
